0ae59ee757b0e33ad4d4b9ff727ff7d3bbf40909
asai <dev.care0717@gmail.com>
2020-02-02 10:30:00 +0900

    format

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,2 +1,2 @@
 export const range = (start: number, end: number) =>
-    Array.from({ length: end - start + 1 }, (v, k) => k + start);
+  Array.from({ length: end - start + 1 }, (v, k) => k + start);
